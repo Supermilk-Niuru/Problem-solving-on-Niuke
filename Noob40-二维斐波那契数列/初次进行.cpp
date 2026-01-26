7f2d414e88a3bd07b01fc52f895bf045dbd3f4f9
liang-hong666 <Ubermilk666@163.com>
2026-01-26 22:00:11 +0800

    Noob-40

---
+++ Noob40-二维斐波那契数列/初次进行.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+static int a[1001][1001];
+int main()
+{
+    int i,j,m,n;
+    cin>>i;
+    cin>>j;
+    a[1][1]=1;
+    for(m=2;m<=i;m++)
+    {
+        a[m][1]=a[m-1][1];
+    }
+    for(n=2;n<=j;n++)
+    {
+        a[1][n]=a[1][n-1];
+    }
+    if(i==1&&j==1)
+    {
+        cout<<a[i][j]%(int)(1e9+7);
+    }
+    else if(i>=2&&j==1)
+    {
+        cout<<a[i][j]%(int)(1e9+7);
+    }
+    else if(i==1&&j>=2)
+    {
+        cout<<a[i][j]%(int)(1e9+7);
+    }
+    else if(i>=2&&j>=2)
+    {
+        for(m=2;m<=i;m++)
+        {
+            for(n=2;n<=j;n++)
+            {
+                a[m][n]=(a[m-1][n]+a[m][n-1])%(int)(1e9+7);
+            }
+        }
+        cout<<a[i][j];
+    }
+}
